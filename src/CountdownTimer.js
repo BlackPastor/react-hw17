c4b212ccc0d6748ede91949cfaf88421ed4568ae
Serhii <sergii.ippolitov@gmail.com>
2022-11-06 10:17:03 +0200

    fixed intervals work

--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -6,62 +6,65 @@ class CountdownTimer extends Component {
         this.state = {
             firstTimer: 50,
             secondTimer: 120,
-            autostart: false,
+            autostartFirstTimer: false,
+            autostartSecondTimer: false,
             firstTimerLine: 400,
             secondTimerLine: 400,
+            firstInterval: this.firstInterval,
+            secondInterval: this.secondInterval,
         }
     }
     componentDidMount() {
-        const { autostart } = this.state;
-        if (autostart) {
-            this.timer();
+        const { autostartFirstTimer, autostartSecondTimer } = this.state;
+        if (autostartFirstTimer) {
+            this.timerUp();
+        } if (autostartSecondTimer) {
+            this.timerDown()
         }
     }
 
     timerUp = () => {
-
-        this.state.autostart = !this.state.autostart;
-        if (this.state.autostart) {
-            this.interval = setInterval(() => {
-                const { firstTimer, firstTimerLine, firstTimerLineOnTime } = this.state;
+        this.state.autostartFirstTimer = !this.state.autostartFirstTimer;
+        if (this.state.autostartFirstTimer) {
+            this.firstInterval = setInterval(() => {
+                const { firstTimer, firstTimerLine } = this.state;
                 this.setState({
                     firstTimer: firstTimer - 1,
                     firstTimerLine: firstTimerLine - (firstTimerLine / firstTimer)
                 })
                 document.querySelector('.firstLine').style.width = firstTimerLine - (firstTimerLine / firstTimer) + 'px';
-               
+
             }, 1000)
 
-        } if (!this.state.autostart) {
-            clearInterval(this.interval)
+        } if (!this.state.autostartFirstTimer) {
+            clearInterval(this.firstInterval)
         }
     }
     timerDown = () => {
-        this.state.autostart = !this.state.autostart;
-        if (this.state.autostart) {
-            this.interval = setInterval(() => {
-                let { secondTimer, secondTimerLine, secondTimerLineOnTime } = this.state;
+        this.state.autostartSecondTimer = !this.state.autostartSecondTimer;
+        if (this.state.autostartSecondTimer) {
+            this.secondInterval = setInterval(() => {
+                let { secondTimer, secondTimerLine } = this.state;
                 this.setState({
                     secondTimer: secondTimer - 2,
                     secondTimerLine: secondTimerLine - (secondTimerLine / secondTimer)
                 })
                 document.querySelector('.secondLine').style.width = secondTimerLine - (secondTimerLine / secondTimer) + 'px';
             }, 2000)
-        } if (!this.state.autostart) {
-            clearInterval(this.interval)
+        } if (!this.state.autostartSecondTimer) {
+            clearInterval(this.secondInterval)
         }
     }
-    componentDidUpdate(prevState) {
-        if (prevState.firstTimer !== this.state.firstTimer && prevState.secondTimer !== this.state.secondTimer && this.state.firstTimer === 0 && this.state.secondTimer === 0) {
-            clearInterval(this.timer);
-            if (this.props.onTimesup) {
-                this.props.onTimesup();
-            }
+    componentDidUpdate() {
+        if (!this.state.firstTimer || this.state.firstTimer === 0) {
+            clearInterval(this.timerUp);
+        } else if (this.state.secondTimer || this.state.secondTimer === 0) {
+            clearInterval(this.timerDown);
         }
     }
     fmtMSS(s) { if (s >= 0) { return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s } else return (s = "0.00") }
     render() {
-        let { firstTimer, secondTimer, firstTimerLine, secondTimerLine } = this.state;
+        let { firstTimer, secondTimer } = this.state;
         return (
             <>
                 {this.fmtMSS(firstTimer)}
